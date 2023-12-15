b69840086fc62563aaedfb9d1f27606ee6bf36b6
Yamilet Jerez <quieromiwww@gmail.com>
2023-12-15 15:07:16 +0100

    -

--- public/store/bears.store.ts
+++ public/store/bears.store.ts
@@ -1,5 +1,10 @@
 import { create } from 'zustand';
 
-export const useBearsStore = create(() => ({
+interface BearsState {
+    bears: number;
+    increasePopulation: () => void;
+}
+export const useBearsStore = create<BearsState>((set) => ({
     bears: 0,
+    increasePopulation: () => set((state) => ({ bears: state.bears + 1}))
 }));
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+"use client"
+import { useBearsStore } from "../../public/store/bears.store";
 const HomePage = () => {
+  const bears = useBearsStore((state) => state.bears)
   return (
     <div><h1>
       Home page
       </h1>
+      <p>Bears: {bears}</p>
       </div>
-  )
-}
+  );
+};
 
 export default HomePage;
